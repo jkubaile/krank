bb18837c09aaed3b2e3e1c804c951be3b7bc2c16
Jörg Kubaile <joerg@kubaile.de>
2018-02-08 21:44:35 +0100

    some ember

---
+++ frontend/app/controllers/index.js
@@ -0,0 +1,30 @@
+import Controller from '@ember/controller';
+import { task, timeout } from 'ember-concurrency';
+import { inject } from "@ember/service";
+import { get, set } from "@ember/object";
+import { computed } from "@ember/object";
+
+export default Controller.extend({
+
+    ajax: inject(),
+    
+    fetchTable: task(function * () {
+        const ajax = get(this, 'ajax');
+        let table = yield ajax.request("http://kicker.ding.si/table");
+        set(this, 'table', table);
+    }).on('init'),
+
+    sortedTable: computed('table', function() {
+        let unsorted = [];
+        const table = get(this, 'table');
+        for(let key in table) {
+            unsorted.pushObject({
+                name: key,
+                value: table[key],
+                url: `http://kicker.ding.si/avatars/${key}.jpeg`
+            });
+        }
+        return unsorted.sortBy('value').reverse(); 
+    })
+
+});
---
+++ frontend/app/routes/index.js
@@ -0,0 +1,13 @@
+import Route from "@ember/routing/route";
+import { inject } from "@ember/service";
+import { get } from "@ember/object";
+
+export default Route.extend({
+    // ajax: inject(),
+    
+    // async model() {
+    //     const ajax = get(this, 'ajax');
+    //     const data = await ajax.request("http://kicker.ding.si/table");
+    // }
+    
+});
