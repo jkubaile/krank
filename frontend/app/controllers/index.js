dac3b5b525c5116117f2312a9de679a7cb86de15
Jörg Kubaile <joerg@kubaile.de>
2018-02-11 14:52:34 +0100

    playing with results

--- frontend/app/controllers/index.js
+++ frontend/app/controllers/index.js
@@ -1,5 +1,5 @@
 import Controller from '@ember/controller';
-import { task, timeout } from 'ember-concurrency';
+import { task } from 'ember-concurrency';
 import { inject } from "@ember/service";
 import { get, set } from "@ember/object";
 import { computed } from "@ember/object";
@@ -14,6 +14,39 @@ export default Controller.extend({
         set(this, 'table', table);
     }).on('init'),
 
+    fetchLogs: task(function * () {
+        const ajax = get(this, 'ajax');
+        let logs = yield ajax.request(
+            "https://kicker.ding.si/logs",
+            { dataType: 'text' }
+        );
+        set(this, 'logs', logs);
+    }).on('init'),
+
+    formatedLogs: computed('logs', function() {
+        let formatedLogs = [];
+        const logs = get(this, 'logs').split("\n");
+        logs.forEach((log) => {
+            if (log) {
+                log = log.replace('<br>', '').split(' - ');
+                let timestamp = log[0];
+                let paaring = log[1].split(' defeat ');
+                let winners = paaring[0].split('+');
+                let loosers = paaring[1].split('+');
+
+                let winner1Url = `https://kicker.ding.si/avatars/${winners[0]}.jpeg`;
+                let winner2Url = `https://kicker.ding.si/avatars/${winners[1]}.jpeg`;
+                let looser1Url = `https://kicker.ding.si/avatars/${loosers[0]}.jpeg`;
+                let looser2Url = `https://kicker.ding.si/avatars/${loosers[1]}.jpeg`;
+
+                formatedLogs.pushObject({ timestamp, winners, loosers, winner1Url, winner2Url,
+                                          looser1Url, looser2Url });
+            }
+        });
+        console.log(formatedLogs);
+        return formatedLogs
+    }),
+
     sortedTable: computed('table', function() {
         let unsorted = [];
         const table = get(this, 'table');
@@ -21,7 +54,7 @@ export default Controller.extend({
             unsorted.pushObject({
                 name: key,
                 value: table[key],
-                url: `http://kicker.ding.si/avatars/${key}.jpeg`
+                url: `https://kicker.ding.si/avatars/${key}.jpeg`
             });
         }
         return unsorted.sortBy('value').reverse();
